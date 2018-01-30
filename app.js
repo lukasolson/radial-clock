4074cd1f736aa6684c1d5cd621934a6d33550f79
Lukas Olson <olson.lukas@gmail.com>
2018-01-30 10:47:26 -0700

    Use different colors, remove dependencies, refactor

--- app.js
+++ app.js
@@ -1,42 +1,34 @@
-$(document).ready(function() {
-	var canvas = document.getElementById("canvas"),
-		context = canvas.getContext("2d");
-	
+(function () {
+	const canvas = document.getElementById("canvas");
+	const context = canvas.getContext("2d");
+
 	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
 
 	context.lineCap = "round";
-	context.lineWidth = 20;
+	context.lineWidth = Math.min(canvas.width, canvas.height) / 9;
 	context.translate(canvas.width / 2, canvas.height / 2); // Center the origin
 	context.rotate(-Math.PI / 2); // Start the circle at 0:00 instead of 3:00
 
-	setInterval(function () {
-		context.clearRect(-canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height);
+	window.requestAnimationFrame(function draw() {
+		context.clearRect(-canvas.height / 2, -canvas.width / 2, canvas.height, canvas.width); // Invert x/y because of the rotation
 
-		var date = new Date(),
-			msecs = date.getMilliseconds(),
-			secs = date.getSeconds() + msecs / 1000,
-			mins = date.getMinutes() + secs / 60,
-			hours = date.getHours() + mins / 60;
+		const now = new Date();
+		getArcLengths(now).forEach((value, i, list) => {
+			context.strokeStyle = `hsl(${i / list.length * 360}, 90%, 70%)`;
+			context.beginPath();
+			context.arc(0, 0, context.lineWidth * (i + 1), 0, (2 * Math.PI) * value);
+			context.stroke();
+		});
 
-		context.strokeStyle = "rgb(128, 128, 255)";
-		context.beginPath();
-		context.arc(0, 0, 100, 0, (2 * Math.PI) * hours / 24);
-		context.stroke();
+		window.requestAnimationFrame(draw);
+	});
 
-		context.strokeStyle = "rgb(128, 255, 128)";
-		context.beginPath();
-		context.arc(0, 0, 75, 0, (2 * Math.PI) * mins / 60);
-		context.stroke();
-
-		context.strokeStyle = "rgb(255, 128, 128)";
-		context.beginPath();
-		context.arc(0, 0, 50, 0, (2 * Math.PI) * secs / 60);
-		context.stroke();
-
-		context.strokeStyle = "rgb(255, 255, 128)";
-		context.beginPath();
-		context.arc(0, 0, 25, 0, (2 * Math.PI) * msecs / 1000);
-		context.stroke();
-	}, 1000 / 48); // 48fps baby!
-});
+	function getArcLengths(date) {
+		const msecs = date.getMilliseconds() / 1000;
+		const secs = (date.getSeconds() + msecs) / 60;
+		const mins = (date.getMinutes() + secs) / 60;
+		const hours = (date.getHours() + mins) / 24;
+		return [msecs, secs, mins, hours];
+	}
+})();
